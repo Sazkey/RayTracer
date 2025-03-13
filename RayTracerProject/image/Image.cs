382975b397d5bd55f95e983ede5d6b09026f16c8
Sazkey <Sazkeyy@gmail.com>
2025-03-13 01:00:41 +0100

    initialize project

---
+++ RayTracerProject/image/Image.cs
@@ -0,0 +1,14 @@
+﻿namespace RayTracerProject.image;
+
+public class Image
+{
+    private char _white = '.';
+    private char _black = 'x';
+
+    public char[,] ExampleImage = new char[64,48];
+
+    private (int, int) ImageCentre(char[,] image)
+    {
+        
+    }
+}
