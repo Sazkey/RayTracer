90d2c94c6b67ec45981a72d1d220a3595ff536b8
Sazkey <Sazkeyy@gmail.com>
2025-03-13 17:35:36 +0100

    Add 2d Vector2.cs class

---
+++ RayTracerProject/image/Vector2.cs
@@ -0,0 +1,14 @@
+﻿namespace RayTracerProject.image;
+
+public class Vector2
+{
+    public int X { get; }
+    public int Y { get; }
+    public double EuclideanNorm => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+
+    public Vector2(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
