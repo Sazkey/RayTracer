5e6639903f1193ea309aa14f66cbb95f1849eef4
Sazkey <Sazkeyy@gmail.com>
2025-03-25 21:22:06 +0100

    add Screen.cs object

---
+++ RayTracerProject/sceneObjects/Screen.cs
@@ -0,0 +1,10 @@
+﻿using RayTracerProject.vector;
+
+namespace RayTracerProject.sceneObjects;
+
+public class Screen(int width, int height, Vector3 position)
+{
+    public int Width { get; } = width;
+    public int Height { get; } = height;
+    public Vector3 Position { get; } = position;
+}
