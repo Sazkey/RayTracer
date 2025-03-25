08b10a9e23fb717fea80cc28baf017af07fafafd
Sazkey <Sazkeyy@gmail.com>
2025-03-25 20:33:13 +0100

    add Camera.cs class

---
+++ RayTracerProject/image/Camera.cs
@@ -0,0 +1,8 @@
+﻿namespace RayTracerProject.image;
+
+public class Camera(Vector3 position, Vector3 viewPoint)
+{
+    public Vector3 Position { get; } = position;
+    public Vector3 ViewPoint { get; } = viewPoint;
+    public Vector3 ViewDirection => new(ViewPoint.X - Position.X, ViewPoint.Y - Position.Y, ViewPoint.Z - Position.Z);
+}
