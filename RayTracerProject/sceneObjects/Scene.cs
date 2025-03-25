a98a4996e8e503e516b35cc19a85f007e171d530
Sazkey <Sazkeyy@gmail.com>
2025-03-25 21:22:45 +0100

    create Scene.cs consisting of scene objects

---
+++ RayTracerProject/sceneObjects/Scene.cs
@@ -0,0 +1,9 @@
+﻿namespace RayTracerProject.sceneObjects;
+
+public class Scene(Sphere sphere, Camera camera, double focalDistance, Screen screen)
+{
+    public Sphere Sphere { get; } = sphere;
+    public Camera Camera { get; } = camera;
+    public double FocalDistance { get; } = focalDistance;
+    public Screen Screen { get; } = screen;
+}
