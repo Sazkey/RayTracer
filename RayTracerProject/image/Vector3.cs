bede60930ab6fdc0a60cd35247a08ba991bec5e5
Sazkey <Sazkeyy@gmail.com>
2025-03-25 20:32:42 +0100

    add 3D Vector class

---
+++ RayTracerProject/image/Vector3.cs
@@ -0,0 +1,12 @@
+﻿namespace RayTracerProject.image;
+
+public class Vector3(double x, double y, double z)
+{
+    public double X { get; } = x;
+    public double Y { get; } = y;
+    public double Z { get; } = z;
+    public double EuclideanNorm => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+    public double DotProduct(Vector3 secondVector) => X * secondVector.X + Y * secondVector.Y + Z * secondVector.Z;
+    public Vector3 Scaled(double scalar) => new Vector3(scalar * X, scalar * Y, scalar * Z);
+    public Vector3 Sum(Vector3 addend) => new Vector3(addend.X + X, addend.Y + Y, addend.Z + Z);
+}
