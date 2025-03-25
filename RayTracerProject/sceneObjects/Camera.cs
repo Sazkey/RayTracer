3c72db3a90c2d87a5437157c6b0f94cdfa4b15cc
Sazkey <Sazkeyy@gmail.com>
2025-03-25 21:21:47 +0100

    move SceneObjects to new Directory

--- RayTracerProject/sceneObjects/Camera.cs
+++ RayTracerProject/sceneObjects/Camera.cs
@@ -1,7 +1,12 @@
-﻿namespace RayTracerProject.image;
+﻿using RayTracerProject.image;
+using RayTracerProject.vector;
+
+namespace RayTracerProject.sceneObjects;
 
 public class Camera(Vector3 position, Vector3 viewPoint)
 {
+    public Vector3 CameraUp = new(0, 1, 0);
+    public Vector3 CameraRight = new(1, 0, 0);
     public Vector3 Position { get; } = position;
     public Vector3 ViewPoint { get; } = viewPoint;
     public Vector3 ViewDirection => new(ViewPoint.X - Position.X, ViewPoint.Y - Position.Y, ViewPoint.Z - Position.Z);
---
+++ RayTracerProject/sceneObjects/Sphere.cs
@@ -0,0 +1,13 @@
+﻿using RayTracerProject.vector;
+
+namespace RayTracerProject.sceneObjects;
+
+public class Sphere(Vector3 origin, double radius)
+{
+    public Vector3 Origin { get; } = origin;
+    private double Radius { get; } = radius;
+    public bool WithinSphere(Vector3 vector)
+    {
+        return vector.DotProduct(vector) <= Math.Pow(Radius, 2);
+    }
+}
